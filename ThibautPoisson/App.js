63dbd7e9db116acd0e9d58773348f11273dc2af6
Natalia Gizila <natalia.gizila@etu.univ-grenoble-alpes.fr>
2021-10-18 16:52:24 +0200

    correction dans les imports

--- ThibautPoisson/App.js
+++ ThibautPoisson/App.js
@@ -3,7 +3,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button } from 'react-native';
-import Home from './screens/home';
+
 
 
 const Stack = createStackNavigator();
