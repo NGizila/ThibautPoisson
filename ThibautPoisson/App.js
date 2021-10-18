9dac9bb258f638810872a1dc263ae642374b073e
Natalia Gizila <natalia.gizila@etu.univ-grenoble-alpes.fr>
2021-10-18 16:30:25 +0200

    preparation des screens et contenu

--- ThibautPoisson/App.js
+++ ThibautPoisson/App.js
@@ -4,10 +4,16 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <NavigationContainer>
+    //   <Stack.Navigator>
+    //   <Stack.Screen name="Page d'accueil" component={Home} options={{titile: 'Welcome'}} />
+    //     <Stack.Screen name="Promotion" component={Promotion} options={{titile: 'Promotion'}} />
+    //     <Stack.Screen name="Bateau" component={Bateau} options={{titile: 'Bateau'}} />
+    //     <Stack.Screen name="Resto" component={Restaurant} options={{titile: 'Resto'}} />
+    //     <Stack.Screen name="Recettes" component={Recettes} options={{titile: 'Recettes'}} />
+    //     <Stack.Screen name="Contacte" component={AddFriends} options={{titile: 'Contacte'}} />
+    //   </Stack.Navigator>
+    // </NavigationContainer>
   );
 }
 
