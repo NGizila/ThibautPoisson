0b2a5095953169aa3b8f3b2b58c101a19d668af7
Natalia Gizila <natalia.gizila@etu.univ-grenoble-alpes.fr>
2021-10-19 08:41:53 +0200

    création du table avec tous les onglets necessaires

--- ThibautPoisson/App.js
+++ ThibautPoisson/App.js
@@ -1,19 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './screens/home';
+
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    // <NavigationContainer>
-    //   <Stack.Navigator>
-    //   <Stack.Screen name="Page d'accueil" component={Home} options={{titile: 'Welcome'}} />
-    //     <Stack.Screen name="Promotion" component={Promotion} options={{titile: 'Promotion'}} />
-    //     <Stack.Screen name="Bateau" component={Bateau} options={{titile: 'Bateau'}} />
-    //     <Stack.Screen name="Resto" component={Restaurant} options={{titile: 'Resto'}} />
-    //     <Stack.Screen name="Recettes" component={Recettes} options={{titile: 'Recettes'}} />
-    //     <Stack.Screen name="Contacte" component={AddFriends} options={{titile: 'Contacte'}} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Page d'accueil" component={Home} options={{titile: 'Welcome'}} />
+        {/* <Stack.Screen name="Promotion" component={Promotion} options={{titile: 'Promotion'}} />
+        <Stack.Screen name="Bateau" component={Bateau} options={{titile: 'Bateau'}} />
+        <Stack.Screen name="Resto" component={Restaurant} options={{titile: 'Resto'}} />
+        <Stack.Screen name="Recettes" component={Recettes} options={{titile: 'Recettes'}} />
+        <Stack.Screen name="Contacte" component={AddFriends} options={{titile: 'Contacte'}} /> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
