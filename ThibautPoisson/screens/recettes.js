ec7458812755d23fdb58ee5bd8da9f14861502b5
Natalia Gizila <natalia.gizila@etu.univ-grenoble-alpes.fr>
2021-10-19 16:49:15 +0200

    ajout des recettes et d'un ficher json dans constants

--- ThibautPoisson/screens/recettes.js
+++ ThibautPoisson/screens/recettes.js
@@ -1,26 +1,58 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView, FlatList,StatusBar } from 'react-native';
 import { Button, Card, Title } from 'react-native-paper';
 
 export default class Recettes extends React.Component {
-    constructor(props){
-        super(props)    
-      }
     render() {
-   
+    const recettes = require('../constants/recettes.json')
+    
+    const Item = ({ name }) => (
+        <View style={styles.item}>
+          <Text style={styles.title}>{name}</Text>
+        </View>
+      );
+
+    const renderItem = ({ item }) => (
+        <Item name={item.name} />
+      );
+
      return(
        <View style={styles.container}>
-         <Text> LOL </Text>
+         {/* {jsonData} */}
+         
+        <SafeAreaView style={styles.container}>
+            <FlatList
+                data={recettes}
+                renderItem={renderItem}
+                keyExtractor={item => item.name}
+                numColumns={2}
+            />
+        </SafeAreaView>
+         
        </View>
      )
     }
    }
-   
-   const styles = StyleSheet.create({
-     container: {
-       justifyContent: "center",
-       alignItems: "center",
-       flex: 1
-       // flexWrap: 'wrap',
-     },
-   })
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: StatusBar.currentHeight,
+    },
+
+    item: {
+      backgroundColor: 'white',//'#f9c2ff',
+      padding: 20,
+      marginVertical: 8,
+      marginHorizontal: 16,
+    },
+    title: {
+        flex: 1,
+        width: 300,
+        flexDirecton: 'row',
+    
+    },
+    });
+    
